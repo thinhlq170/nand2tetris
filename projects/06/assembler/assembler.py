e23a4af338f0878b7a62da1b2f22dd40833e010d
thinhlq170 <thinh.lq170@gmail.com>
2024-07-13 15:23:51 +0700

    update assembler

--- projects/06/assembler/assembler.py
+++ projects/06/assembler/assembler.py
@@ -6,22 +6,64 @@ class Parser:
     A Parser module that parses the input.
     """
     
+    
+    
     def __init__(self, filePath):
+        self.code = Code()
+        self.symbolTable = SymbolTable()
         self.commandType = None
         self.currentOffset = 0
         self.currentCommand = None
         self.lines = []
+        self.result = []
+        self.currentLine = 0
         try:
             with open(filePath, 'r') as file_stream:
                 for line in file_stream:
                     line = line.strip()
                     # if the line is a comment then skip the line
                     if not line.startswith('//'):
-                        self.lines.append(line.strip())
+                        self.lines.append(line)
         except FileNotFoundError:
             print(f"Error: The file {filePath} was not found.")
         except IOError:
             print(f"Error: Could not read the file {filePath}")
+            
+        self.pass1(self)
+        self.pass2(self)
+            
+    def pass1(self):
+        while (self.hasMoreCommand()):
+            if (self.commandType() == CommandType.A_COMMAND) or (self.commandType() == CommandType.C_COMMAND):
+                self.currentLine += 1
+            
+            if self.commandType() == CommandType.L_COMMAND:
+                table = self.symbolTable
+                symbol = self.symbol(self)
+                table.addEntry(symbol, self.currentLine + 1)
+            self.advance()
+        
+        # reset current offset to the first point in the assembly file after completing pass1    
+        self.currentOffset = 0    
+    
+    def pass2(self):
+        while (self.hasMoreCommand()):
+            if self.commandType() == CommandType.C_COMMAND:
+                self.currentLine += 1
+                code = self.code
+                dest = self.dest()
+                comp = self.comp()
+                jump = self.jump()
+                commandPrefix = '111'
+                binDest = code.dest(dest)
+                binComp = code.comp(comp)
+                binJump = code.jump(jump)
+                binCommand = f'{commandPrefix}{binComp}{binDest}{binJump}'
+                self.result.append(binCommand)
+            elif self.commandType == CommandType.A_COMMAND:
+                # TODO: complete pass2 for A_COMMAND
+                pass
+            self.advance()
 
     def hasMoreCommand(self) -> bool:
         """
@@ -47,7 +89,7 @@ class Parser:
         """
         Returns the type of the current command:
         1. A_COMMAND for @Xxx where Xxx is either a symbol or a decimal number
-        2. C_COMMAND for dest=comp;jump
+        2. C_COMMAND for dest=comp;jump. Either the dest or jump fields may be empty
         3. L_COMMAND (actually, pseudocommand) for (Xxx) where Xxx is a symbol.
         """
         commandPattern = r'(?://)?.*$'
@@ -55,15 +97,15 @@ class Parser:
         command = re.search(commandPattern, self.currentCommand)
         if command:
             commandPatternA = "^@.*"
-            commandPatternC = ".*=.*;.*"
+            commandPatternC = ".*=?.*;?.*"
             commandPatternL = "\(.*\).*"
             
             if re.match(commandPatternA, command):
-                self.commandType = CommandType.A_COMMAND
+                return CommandType.A_COMMAND
             elif re.match(commandPatternC, command):
-                self.commandType = CommandType.C_COMMAND
+                return CommandType.C_COMMAND
             elif re.match(commandPatternL, command):
-                self.commandType = CommandType.L_COMMAND
+                return CommandType.L_COMMAND
         
     def symbol(self) -> str:
         """
@@ -78,7 +120,7 @@ class Parser:
                 if match:
                     return match.group(1)
             elif self.commandType == CommandType.L_COMMAND:
-                commandPatternL = "^\(.*\).*"
+                commandPatternL = r'^\(.*\).*'
                 match = re.search(commandPatternL, self.currentCommand)
                 if match:
                     return match.group(1)
@@ -91,7 +133,7 @@ class Parser:
             Should be called only when commandType() is C_COMMAND.
         """
         if self.commandType == CommandType.C_COMMAND:
-            commandPatternC = r'(.*)\s*=\s*(.*)\s*;\s*(.*)$'
+            commandPatternC = r'(.*)\s*=.*;?.*$'
             match = re.search(commandPatternC, self.currentCommand)
             if match:
                 return match.group(1)
@@ -103,10 +145,10 @@ class Parser:
             Should be called only when commandType() is C_COMMAND.
         """
         if self.commandType == CommandType.C_COMMAND:
-            commandPatternC = r'(.*)\s*=\s*(.*)\s*;\s*(.*)$'
+            commandPatternC = r'.*=?\s*(.*)\s*;?.*$'
             match = re.search(commandPatternC, self.currentCommand)
             if match:
-                return match.group(2)
+                return match.group(1)
     
     def jump(self) -> str:
         """
@@ -115,10 +157,10 @@ class Parser:
             Should be called only when commandType() is C_COMMAND.
         """
         if self.commandType == CommandType.C_COMMAND:
-            commandPatternC = r'(.*)\s*=\s*(.*)\s*;\s*(.*)$'
+            commandPatternC = r'.*=?.*;\s*(.*)$'
             match = re.search(commandPatternC, self.currentCommand)
             if match:
-                return match.group(3)
+                return match.group(1)
     
 
 
@@ -130,19 +172,121 @@ class Code:
         """
         Returns the binary code of the dest mnemonic. (3 bits)
         """
-        pass
+        try:
+            if mnemonic == 'null' or mnemonic is None:
+                return '000'
+            elif mnemonic == 'M':
+                return '001'
+            elif mnemonic == 'D':
+                return '010'
+            elif mnemonic == 'MD':
+                return '011'
+            elif mnemonic == 'A':
+                return '100'
+            elif mnemonic == 'AM':
+                return '101'
+            elif mnemonic == 'AD':
+                return '110'
+            elif mnemonic == 'AMD':
+                return '111'
+            else:
+                raise InvalidCommandException
+        except InvalidCommandException:
+            print("Exception occured: Invalid 'dest' Command")
+            
     
     def comp(mnemonic: str):
         """
         Returns the binary code of the comp mnemonic. (7 bits)
         """
-        pass
+        try:
+            if mnemonic == '0':
+                return '0101010'
+            elif mnemonic == '1':
+                return '0111111'
+            elif mnemonic == '-1':
+                return '0111010'
+            elif mnemonic == 'D':
+                return '0001100'
+            elif mnemonic == 'A':
+                return '0110000'
+            elif mnemonic == 'M':
+                return '1110000'
+            elif mnemonic == '!D':
+                return '0001101'
+            elif mnemonic == '!A':
+                return '0110001'
+            elif mnemonic == '!M':
+                return '1110001'
+            elif mnemonic == '-D':
+                return '0001111'
+            elif mnemonic == '-A':
+                return '0110011'
+            elif mnemonic == '-M':
+                return '1110011'
+            elif mnemonic == 'D+1':
+                return '0011111'
+            elif mnemonic == 'A+1':
+                return '0110111'
+            elif mnemonic == 'M+1':
+                return '1110111'
+            elif mnemonic == 'D-1':
+                return '0001110'
+            elif mnemonic == 'A-1':
+                return '0110010'
+            elif mnemonic == 'M-1':
+                return '1110010'
+            elif mnemonic == 'D+A':
+                return '0000010'
+            elif mnemonic == 'D+M':
+                return '1000010'
+            elif mnemonic == 'D-A':
+                return '0010011'
+            elif mnemonic == 'D-M':
+                return '1010011'
+            elif mnemonic == 'A-D':
+                return '0000111'
+            elif mnemonic == 'M-D':
+                return '1000111'
+            elif mnemonic == 'D&A':
+                return '0000000'
+            elif mnemonic == 'D&M':
+                return '1000000'
+            elif mnemonic == 'D|A':
+                return '0010101'
+            elif mnemonic == 'D|M':
+                return '1010101'
+            else:
+                raise InvalidCommandException
+        except InvalidCommandException:
+            print("Exception occured: Invalid 'comp' Command")
     
     def jump(mnemonic: str):
         """
         Returns the binary code of the jump mnemonic. (3 bits)
         """
-        pass
+        
+        try:
+            if mnemonic == 'null' or mnemonic is None:
+                return '000'
+            elif mnemonic == 'JGT':
+                return '001'
+            elif mnemonic == 'JEQ':
+                return '010'
+            elif mnemonic == 'JGE':
+                return '011'
+            elif mnemonic == 'JLT':
+                return '100'
+            elif mnemonic == 'JNE':
+                return '101'
+            elif mnemonic == 'JLE':
+                return '110'
+            elif mnemonic == 'JMP':
+                return '111'
+            else:
+                raise InvalidCommandException
+        except InvalidCommandException:
+            print("Exception occured: Invalid 'jump' Command")
 
 
 class SymbolTable:
@@ -154,29 +298,44 @@ class SymbolTable:
         """
         Creates a new empty symbol table.
         """
-        self.symbolTable = {}
+        self.table = {}
+        for i in range(0, 16):
+            self.addEntry('R' + str(i), i)
+            
+        self.addEntry('SP', 0)
+        self.addEntry('LCL', 1)
+        self.addEntry('ARG', 2)
+        self.addEntry('THIS', 3)
+        self.addEntry('THAT', 4)
+        self.addEntry('SCREEN', 16384)
+        self.addEntry('KBD', 24576)
+        
         
     def addEntry(self, symbol: str, address: int):
         """
         Adds the pair (symbol, address) to the table
         """
-        self.symbolTable[symbol] = address
+        self.table[symbol] = address
         
     def contains(self, symbol: str):
         """
         Does the symbol table contain the given symbol?
         """
-        return symbol in self.symbolTable
+        return symbol in self.table
     
     def getAddress(self, symbol: str):
         """
         Returns the address associated with the symbol.
         """
         if self.contains(symbol):
-            return self.symbolTable.get(symbol)
+            return self.table.get(symbol)
 
 class CommandType(Enum):
     A_COMMAND = 1
     C_COMMAND = 2
-    L_COMMAND = 3        
+    L_COMMAND = 3
+    
+class InvalidCommandException(Exception):
+    "The command is not supported!"
+    pass    
         
--- projects/06/assembler/main.py
+++ projects/06/assembler/main.py
@@ -1,7 +1,14 @@
 from assembler import Parser, Code, SymbolTable
+import argparse
 
 def main():
-    print("Hello World")
+    parser = argparse.ArgumentParser(description="HACK Assembler")
+    parser.add_argument('--path', required=True, help="path of the assembly file.")
+    
+    args = parser.parse_args()
+    hackParser = Parser(args.path)
+    
+    
 
 if __name__ == "__main__":
     main()
